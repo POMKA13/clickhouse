fb29ef5715e0c642ec8f87ecb2716a037bb74b9d
POMKA13 <lisachenkoroma2009@mail.com>
2022-11-12 18:13:51 +0200

    12.11.2022

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.querySelector(".header__menu").style.display = "block";
+    console.log('24234')
+}
+
+function closeNav() {
+    document.querySelector(".header__menu").style.display = 'none'
+}
